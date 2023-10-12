7b2148e092116e30948cf44a1067cccf34f0544d
PETER IRUNGU <peteralidante254@gmail.com>
2023-10-12 20:12:47 +0300

    add springboot for web

---
+++ src/main/java/com/group/component/ComponentApplication.java
@@ -0,0 +1,13 @@
+package com.group.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentApplication.class, args);
+	}
+
+}
